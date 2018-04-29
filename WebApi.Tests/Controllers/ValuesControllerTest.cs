9643afa43f159c9b9fbac926e35a99859d504dc5
Nodari Tabunidze <nodari@artway.ca>
2018-04-29 15:34:04 -0700

    created

---
+++ WebApi.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using BusinessEntities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApi;
+using WebApi.Controllers;
+
+namespace WebApi.Tests.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            //Assert.IsNotNull(result);
+            //Assert.AreEqual(2, result.Count());
+            //Assert.AreEqual("value1", result.ElementAt(0));
+            //Assert.AreEqual("value2", result.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            // Act
+            var result = controller.Get(5);
+
+            // Assert
+            //Assert.AreEqual("value", result);
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            // Act
+            //controller.Post("value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Put()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            // Act
+            //controller.Put(5, "value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            // Act
+            controller.Delete(5);
+
+            // Assert
+        }
+    }
+}
